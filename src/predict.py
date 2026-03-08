219de59e1d45e5c48a675f268979ee9ea3cd91db
MITHUN R L <114131835+mithunrl27@users.noreply.github.com>
2026-03-08 19:33:28 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -5,8 +5,8 @@ from sklearn.metrics import classification_report,accuracy_score
 df=load_data()
 x_train,x_test,y_train,y_test=preprocess(df)
 model = joblib.load("model/fraud_model.pkl")
-y_pred_train=model.fit(x_train)
-y_pred_test=model.fit(x_test)
+y_pred_train=model.predict(x_train)
+y_pred_test=model.predict(x_test)
 
 print("Training Accuracy:",accuracy_score(y_pred_train,y_train))
 print("Training Accuracy:",accuracy_score(y_pred_train,y_train))
